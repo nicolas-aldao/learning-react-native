5e5da2491e1ce3f84c09e8e79d8e31df48b0c632
Carlos Nicolás Aldao <caldao@baufest.com>
2022-05-31 17:36:51 -0300

    Trying make work react router

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Main from "./src/components/Main";
+import About from "./src/components/About";
 
 export default function App() {
-  return <Main></Main>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+  );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import StyledText from "./StyledText";
+
+const About = () => {
+  return (
+    <View>
+      <StyledText>YE!!</StyledText>
+    </View>
+  );
+};
+
+export default About;
